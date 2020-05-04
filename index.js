7468d7b3082a554f938309f7cf649cbb7ce59fd8
Devin_Moore <d.j.moore209@gmail.com>
2020-05-04 18:35:21 -0400

    patch endpoint working, logical operations for post and get with ids

--- index.js
+++ index.js
@@ -33,48 +33,85 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/users', function(req, res) {
-    const newName = req.body;
+    let newName = req.body;
+    newName.id = shortid.generate();
 
-    userNames.push(newName);
+    if (newName.name && newName.bio){
 
-    res.status(201).json(newName)
+        userNames.push(newName);
+
+        res.status(201).json(newName)
+
+    } else {
+
+        const errorMessage = { 
+            errorMessage: "Please provide name and bio for the user." 
+        }
+
+        res.status(400).json(errorMessage)
+    }
+
 });
 
 server.get('/api/users', function(req, res) {
     // return an array of lessons (id, name)
-
     res.json(userNames);
 });
 
 
 server.get('/api/users/:id', (req, res) => {
-    const id = Number(req.params.id);
+    const id = req.params.id;
     
     // find the user from array
-    let user = userNames.filter(name => name.id === id);
-    
-    res.status(201).json(user);
+    let [ user ] = userNames.filter(name => name.id === id);
+
+    if(user){
+
+        res.status(201).json(user);
+
+    } else {
+
+        const errorMessage = { 
+            message: "The user with the specified ID does not exist." 
+        }
+
+        res.status(404).json(errorMessage)
+    }
+
 });
 
 server.delete('/api/users/:id', (req, res) => {
     // everything that comes from a url will be a string
-    const id = Number(req.params.id);
+    const id = req.params.id;
 
-    // find the lesson on the array and remove it
-    userNames = userNames.filter(name => name.id !== id)
+    const here = userNames.find(user => user.id == id)
     
-    res.status(200).json(userNames);
+    if (!here){
+
+        const errorMessage = { 
+            message: "The user with the specified ID does not exist." 
+        }
+
+        res.status(404).json(errorMessage)
+    } else {
+
+      // find the lesson on the array and remove it
+      userNames = userNames.filter(name => name.id !== id)
+
+      res.status(200).json(userNames);
+    }
+    
 });
 
 server.patch('/api/users/:id', (req, res) => {
-    const id = Number(req.params.id);
+    const id = req.params.id;
     // updated name
-    const updatedName = req.body.name
+    const update = req.body
     // find user from array
-    const user = userNames.filter(name => name.id === id);
-
-
-
+    const user = userNames.find(name => name.id === id);
+    console.log(user)
+    Object.assign(user, update)
+    console.log(user)
     res.status(201).json( user )
 });
 
